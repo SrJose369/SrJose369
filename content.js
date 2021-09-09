72d8c0016a45cb645f7c8da8efdc4820b1e830bc
Jose <marianoalfonso1233@gmail.com>
2021-09-09 16:48:26 -0300

    content ok con checkeos

--- content.js
+++ content.js
@@ -5,63 +5,106 @@ var canal;
 var cual = document.URL;
 function refre() {
     var a, b, c;
-    if (cual[12] == "y") {
-        msjConsola("Yotubue creo");
-        // tiempo video
-        a = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-current");
-        // nombre canal
-        b = document.querySelector("#text > a");
-        // titulo
-        c = document.querySelector("#container > h1 > yt-formatted-string");
+    try {
+        if (checkNull(cual) || checkNull(cual.length)) {
+            console.log("Error cual vacio");
+        }else {
+            if (cual.length < 13) {
+                console.log('Error el length de cual es menor a 13');
+            }
+        }
+    } catch (error) {
+        console.warn(error);
     }
-    else {
-        if (cual[12] == "t") {
-            msjConsola("Twitch creo");
-            // tiempo directo
-            a = document.querySelectorAll('[data-a-target="player-seekbar-current-time"]')[0];
-            // canal
-            b = document.getElementsByClassName("CoreText-sc-cpl358-0 ScTitleText-sc-1gsen4-0 cWnsFT InjectLayout-sc-588ddc-0 fwsYSr tw-title")[0];
-            // titulo video
-            c = document.querySelectorAll('[data-a-target="stream-title"]')[0];
+    try {
+        if (cual[12] == "y") {
+            msjConsola("Yotubue creo");
+            // tiempo video
+            a = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-current");
+            // nombre canal
+            b = document.querySelector("#text > a");
+            // titulo
+            c = document.querySelector("#container > h1 > yt-formatted-string");
+        }else {
+            if (cual[12] == "t") {
+                msjConsola("Twitch creo");
+                // tiempo directo
+                a = document.querySelectorAll('[data-a-target="player-seekbar-current-time"]')[0];
+                // canal
+                b = document.getElementsByClassName("CoreText-sc-cpl358-0 ScTitleText-sc-1gsen4-0 cWnsFT InjectLayout-sc-588ddc-0 fwsYSr tw-title")[0];
+                // titulo video
+                c = document.querySelectorAll('[data-a-target="stream-title"]')[0];
+            }else {
+                msjConsola("No es youtbe y tampoco twitch :/");;
+            }
         }
-        else {
-            msjConsola("Nose");
-        }
+    } catch (error) {
+        console.warn(error);
     }
     var xtie, xcan, xtit;
-    if (!a) {
-        msjConsola("String vacio tiempo" + a);
+    try {
+        if (checkNull(a) || checkNull(a.innerText)) {
+            msjConsola("String vacio tiempo" + a);
+            xtie = "?";
+        }
+        else {
+            xtie = a.innerText;
+        }
+    } catch (error) {
+        msjConsola("String error vacio tiempo" + a);
         xtie = "?";
     }
-    else {
-        xtie = a.innerText ? a.innerText : "?";
-    }
-    if (!b) {
-        msjConsola("String vacio canal" + b);
+    try {
+        if (checkNull(b) || checkNull(b.innerText)) {
+            msjConsola("String vacio canal" + b);
+            xcan = "?";
+        }
+        else {
+            xcan = b.innerText;
+        }   
+    } catch (error) {
+        msjConsola("String error vacio canal" + b);
         xcan = "?";
     }
-    else {
-        xcan = b.innerText ? b.innerText : "?";
-    }
-    if (!c) {
-        msjConsola("String vacio titulo" + c);
+    try {
+        if (checkNull(c) || checkNull(c.innerText)) {
+            msjConsola("String vacio titulo" + c);
+            xtit = "?";
+        }
+        else {
+            xtit = c.innerText;
+        }
+    } catch (error) {
+        msjConsola("String error vacio titulo" + c);
         xtit = "?";
-    }
-    else {
-        xtit = c.innerText ? c.innerText : "?";
+
     }
     titu = xtit;
     tiem = xtie;
     canal = xcan;
-    //chrome.storage.sync.set({"vidText": s1, "canal": s2});
 }
 chrome.runtime.onMessage.addListener(function (res, sender, sendResponse) {
-    console.log(res.txt);
-    if (res.txt == "refresh") {
-        refre();
+    try {
+        if (checkNull(res) || checkNull(res.txt)) {
+            msjConsola("Error en el paquete recibido en content");
+        }else {
+            console.log(res.txt);
+            if (res.txt == "refresh") {
+               refre();
+            }
+            sendResponse({titu: titu, tiem: tiem, canal: canal});   
+        }
+    } catch (error) {
+       console.warn(error);
     }
-    sendResponse({titu: titu, tiem: tiem, canal: canal});
 });
 function msjConsola(m) {
     console.log(m);
+}
+function checkNull(valor) {
+    if (valor === undefined || valor === null) {
+        return true;
+    }else {
+        return false;
+    }
 }
